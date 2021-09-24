bf6a5417be4103379396fd7b7399bf07ef0d5d28
Jose Luis Eduardo Cayo Acuña <jcayoacu@everis.com>
2021-09-24 16:07:06 -0500

    Excepciones, Testing y Sqlite

---
+++ starting/testing/test.py
@@ -0,0 +1,15 @@
+# ejecutar test: $ python -m doctest test.py
+def multiplicar (num1, num2):
+    """
+    Funcion que multiplica dos numeros
+    Argumentos:
+    numero1 (int)
+    numero2 (int)
+    Retorna la multiplicacion de los parametros dados.
+    
+    >>> multiplicar(2,3)
+    6
+    >>> multiplicar(2,4)
+    9
+    """
+    return num1*num2
